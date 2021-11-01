b13074da980fbe494df2c5088e51a99d98b1b845
Nik <nikitama1hr@gmail.com>
2021-11-01 23:56:48 +0300

    create auth user and JWT generate

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,1 +1,3 @@
 export const ALREDY_REGISTERED_ERROR = 'Такой пользователен уже зарегистрирован'
+export const USER_NOT_FOUND_ERROR = 'Пользователь не найден'
+export const WRONG_PASSWORD_ERROR = 'Пароль не верный!'
